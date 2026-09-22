44cb55f275f594f64977f88f8d7d31a0c32a8578
Matthew Bernardo <matthew.r.bernardo@gmail.com>
2026-09-22 08:34:17 +0200

    Add alias for fixing chromium singleton

--- src/configs/zsh/aliases.sh
+++ src/configs/zsh/aliases.sh
@@ -21,3 +21,4 @@ alias teardownchat="~/stacksync/bin/run_chat_env.sh --teardown"
 alias yz='kitty yazi'
 alias unafig='cursor ~/.una/una-linux'
 alias juna='cd ~/.una/una-linux'
+alias fixchromium="rm ~/.config/chromium/Singleton*"
